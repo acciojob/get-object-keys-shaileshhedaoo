0f64b436f83e53db650f8d63ebcf61e80d558cb1
shaileshhedaoo <100752372+shaileshhedaoo@users.noreply.github.com>
2024-08-19 11:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Create the student object with a property called 'name'
+const student = {
+  name: 'John Doe'
+};
+
+// Add the getKeys method to Object.prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Use the getKeys method on the student object
+const keys = student.getKeys();
+console.log(keys); // Output: ['name']
